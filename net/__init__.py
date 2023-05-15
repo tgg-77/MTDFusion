abf4ddc5ffeddbc5af771af7d816d5e1e7e118da
tgg-77 <30632209+tgg-77@users.noreply.github.com>
2023-05-15 21:42:29 +0800

    Update __init__.py

--- net/__init__.py
+++ net/__init__.py
@@ -1,6 +1,6 @@
 import torch
 from .densefuse import DenseFuse_net
-from .TDFusion import TDFusion_net
+from .MTDFusion import MTDFusion_net
 from .fusion_layer import Fusion_network
 from .IFCNN import myIFCNN
 
@@ -8,8 +8,8 @@ from .IFCNN import myIFCNN
 def model_generator(method, pretrained_model_path=None):
     if method == 'densefuse':
         model = DenseFuse_net(input_nc=1, output_nc=1).cuda()
-    elif method == 'TDFusion':
-        model = TDFusion_net(input_nc=1, output_nc=1).cuda()
+    elif method == 'MTDFusion':
+        model = MTDFusion_net(input_nc=1, output_nc=1).cuda()
     elif method == 'fusion_layer':
         model = Fusion_network([64], 'res').cuda()
     elif method == 'ifcnn':
